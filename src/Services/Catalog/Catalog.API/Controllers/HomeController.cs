27af643884830d2c9042da767262ad3e33c07359
chenna0501 <chennakesavulu0501@gmail.com>
2023-10-28 22:17:52 +0530

    moving Catalog.API microservice

---
+++ src/Services/Catalog/Catalog.API/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        /*
+        [HttpGet(Name ="GetInt")]
+        public int Get()
+        {
+            return 100;
+        }
+        */
+
+        [HttpGet(Name = "GetbyId")]
+        public int GetValue(int Id)
+        {
+            return Id;
+        }
+    }
+}
